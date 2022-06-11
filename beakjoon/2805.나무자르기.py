ac48ca851933971dcf280788147bb95782efcf41
seungmiKim <kmimi0528@gmail.com>
2022-06-11 17:01:38 +0900

    이분탐색 이용

--- beakjoon/2805.나무자르기.py
+++ beakjoon/2805.나무자르기.py
@@ -1,18 +1,32 @@
 import sys
+
+def cut_tree(trees, height):
+    sum_t=0
+    for t in trees:
+        if t>height:
+            sum_t+=t-height
+    return sum_t
+
+def binary_search(array,target,start, end):
+    while start<=end:
+        height=(start+end)//2
+        tree_sum=cut_tree(array,height)
+        if tree_sum==target:
+            return height
+        elif tree_sum<target:
+            end=height-1
+        else:
+            start=height+1
+            if start>end:
+                return height
+            
+    return height-1
+
+
 n,m=map(int, sys.stdin.readline().rstrip().split())
 trees=list(map(int,sys.stdin.readline().rstrip().split()))
 trees.sort(reverse=True)
-hight=trees[0]
+print(binary_search(trees,m,0,trees[0]))
 
-while True:
-    hight-=1
-    sum_tree=0
-    for t in trees:
-        if t>hight:
-            sum_tree+=t-hight
-    if sum_tree>=m:
-        break
-
-print(hight)
     
 
