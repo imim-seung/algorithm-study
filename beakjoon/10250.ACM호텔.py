5f267c4da1d2a20a8cb80b2099972cc08662569b
seungmiKim <kmimi0528@gmail.com>
2022-06-12 23:42:17 +0900

    미해결

---
+++ beakjoon/10250.ACM호텔.py
@@ -0,0 +1,10 @@
+import sys
+
+n=int(sys.stdin.readline().rstrip())
+for _ in range(n):
+  h,w,guest=map(int,sys.stdin.readline().rstrip().split())
+  room=str(guest%h) if guest%h !=0 else str(h) 
+  num=(guest//h) if guest//h ==1 else  guest//h +1
+  if num <10:
+    room+='0'
+  print(room+str(num))
---
+++ beakjoon/랜선자르기.py
@@ -0,0 +1,12 @@
+
+
+
+# k,n=map(int,input().split)
+# k_list=[]
+# for _ in range(k):
+#   k_list.append(input())
+
+# k_list.sort(reverse=True)
+
+# while k<n:
+  
