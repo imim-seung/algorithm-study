373a63a66d5472c050fe39e8544aaa2d546908ec
imim-seung <kmimi0528@hanmail.net>
2022-06-12 23:52:42 +0900

    통과

--- beakjoon/10250.ACM호텔.py
+++ beakjoon/10250.ACM호텔.py
@@ -4,7 +4,7 @@ n=int(sys.stdin.readline().rstrip())
 for _ in range(n):
   h,w,guest=map(int,sys.stdin.readline().rstrip().split())
   room=str(guest%h) if guest%h !=0 else str(h) 
-  num=(guest//h) if guest//h ==1 else  guest//h +1
+  num=(guest//h) if guest%h ==0 else  guest//h +1
   if num <10:
     room+='0'
   print(room+str(num))
