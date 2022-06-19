7a2f458e0210c028c84a6c19bb1477585a2605ec
seungmiKim <kmimi0528@gmail.com>
2022-06-19 20:36:55 +0900

    미해결

--- beakjoon/17626.FourSquares.py
+++ beakjoon/17626.FourSquares.py
@@ -2,26 +2,17 @@ import sys
 
 n=int(sys.stdin.readline().rstrip())
 squares=[0]*224
-for i in range(1,224):
-  squares[i]=i*i
+dp=[0]*(n+1)
+dp[0]=0
+dp[1]=1
 
-num=[]
-copy_n=n
-start=round(n**0.5)
-while n>0 and len(num)<5:
-  for i in range(start,0,-1):
-    if squares[i]<=n:
-      n-=squares[i]
-      num.append(i)
-  if len(num)>5:
-    start=num[0]-1
-    num=[]
-    n=copy_n
-  if n>0:
-    start=round(n**0.5)
+for i in range(2,n+1,1):
   
 
-print(len(num))
+  dp[i]=1+dp[i-(j**2)]
+
+
+print(dp[n])
 
 
 
